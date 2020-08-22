00e454dcf39b42ae4c2393de38093bd0f7f5663e
Shruti Pabboju <Shruti_Pabboju@gap.com>
2020-08-22 10:14:21 +0530

    States and listening

--- src/App.js
+++ src/App.js
@@ -3,13 +3,45 @@ import './App.css';
 import Person from'./Person/Person';
 
 class App extends Component {
+state = {
+  persons: [
+    {
+      name:'Shruti',
+      age:23
+    },
+    {
+      name: 'Sample',
+      age:29
+    }
+  ]
+}
+
+switchNameHandler = () => {
+  //console.log('was clicked')
+  // Do not do this this.state.persons[0].name = 'Shruti Pabboju'
+  this.setState({
+    persons: [
+    {
+      name:'Shruti Pabboju',
+      age:23
+    },
+    {
+      name: 'Sample',
+      age:22
+    }
+  ]
+}
+)
+}
+
   render() {
     return (
       <div className="App">
         <h1>First Change</h1>
         <p>This is really working!</p>
-        <Person name="Shruti" age="23" />
-        <Person name="Sample" age="22">Children props</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Children props</Person>
       </div>
     );
     // return React.createElement(,,,,);
