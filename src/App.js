552d0189e710dceb3b5cb7944f3f28094cebeb5b
Shruti Pabboju <Shruti_Pabboju@gap.com>
2020-08-22 17:55:41 +0530

    React hooks - sample

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'; //import useState instead of Componenet
 import './App.css';
 import Person from'./Person/Person';
 
-class App extends Component {
-state = {
-  persons: [
-    {
-      name:'Shruti',
-      age:23
-    },
-    {
-      name: 'Sample',
-      age:29
-    }
-  ]
-}
-
-switchNameHandler = () => {
-  //console.log('was clicked')
-  // Do not do this this.state.persons[0].name = 'Shruti Pabboju'
-  this.setState({
+const app = props => { // does not need render
+  const [personsState, setPersonsState] = useState({ // always returns exactly two elements
     persons: [
-    {
-      name:'Shruti Pabboju',
-      age:23
-    },
-    {
-      name: 'Sample',
-      age:22
-    }
-  ]
-}
-)
-}
+      {
+        name:'Shruti',
+        age:23
+      },
+      {
+        name: 'Sample',
+        age:29
+      }
+    ]
+  });
 
-  render() {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
+    //console.log('was clicked')
+    // Do not do this personsState.persons[0].name = 'Shruti Pabboju'
+    setPersonsState({ 
+      persons: [
+      {
+        name:'Shruti Pabboju',
+        age:23
+      },
+      {
+        name: 'Sample',
+        age:22
+      }
+    ]
+  });
+  };
+
     return (
       <div className="App">
         <h1>First Change</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Children props</Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Children props</Person>
       </div>
     );
     // return React.createElement(,,,,);
-  }
 }
 
-export default App;
+export default app;
+
